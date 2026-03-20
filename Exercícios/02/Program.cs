78c68f6faa18774dc03bf06df7fb8b862504a3df
Albino Tchali <albinotchali1@gmail.com>
2026-03-21 00:09:17 +0100

    exercício 2

--- Exercícios/02/Program.cs
+++ Exercícios/02/Program.cs
@@ -9,7 +9,17 @@ namespace _02
     internal class Program
     {
         static void Main(string[] args)
-        {
+        { 
+            //-Tipos de variaveis
+            string nome = "Tchali";
+            int inteiro = 17;
+            double real = 1.70;
+            char caracter = 'a';
+            bool boleano = true;
+
+            //-Constante
+            const int number = 100;
+           
         }
     }
 }
