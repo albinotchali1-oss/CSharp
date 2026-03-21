1a6fd2c1f83376f7c518691b77a6711cd47a3baa
Albino Tchali <albinotchali1@gmail.com>
2026-03-21 08:40:27 +0100

    aula  4 e 5
    Adicionei algumas aulas ao repositório

---
+++ Exercícios/05_Estrutura condicional if-else/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05_Estrutura_condicional_if_else
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("===========verificador de nºs pares e impares==========");
+            Console.WriteLine("digite um numero: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if(number % 2 == 0)
+            {
+                Console.WriteLine("o numero digitado é par! ");
+            }
+            else
+            {
+                Console.WriteLine("o numero é impar! ");
+            }
+        }
+    }
+}
