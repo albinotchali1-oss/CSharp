5ccf216de14619163e47de7e69422247d5fb5f44
Albino Tchali <albinotchali1@gmail.com>
2026-03-21 07:39:32 +0100

    Aula 3
    Entrada e saída de dados

---
+++ Exercícios/03_Input e Output/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03_Input_e_Output
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-----------------Entrada e saída de dados------------------------");
+
+            //Solicitar o nome
+            Console.WriteLine("Digite seu nome: ");
+            string nome = Console.ReadLine();
+
+            //solicitar a idade
+            Console.WriteLine("Digite sua idade: ");
+            int idade = int.Parse(Console.ReadLine());
+
+            //Saída
+            Console.WriteLine("Nome do usuário: " + nome);
+            Console.WriteLine("Idade do usuário: " + idade);
+        }
+    }
+}
